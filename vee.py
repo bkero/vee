dd181840cf7c2d9a30bfe03dcaa05ecf05c76956
Ben Kero <ben.kero@gmail.com>
2013-01-19 05:22:17 +0700

    Standardize debug output format, add start() command, add method to install puppet

--- vee.py
+++ vee.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import stat
 import shlex
 import config
 from subprocess import Popen, PIPE
@@ -27,8 +28,12 @@ def create(container):
         if config.settings['continue_anyway']:
             return()
         exit(1)
+    if container['profile']['lxc_config'] is not "":
+        apply_lxc(container)
+    install_puppet(container)
+    apply_puppet(container)
 
-def start(container):
+def start(container, command=False):
     """Handle the starting up of a container, creating it if it doesn't already exist."""
     # Create the container if it doesn't exist
     if not exists(container):
@@ -38,7 +43,10 @@ def start(container):
 
     args = "%s/lxc-start" % (config.settings['lxc-bindir'])
     args += " -d " # Daemonize
-    args += "-n %s" % (container['name'])
+    args += "-n %s " % (container['name'])
+    if command:
+        args += " -- %s" % (command)
+    print(args)
     try:
         p = Popen(shlex.split(args), stdout=PIPE, stdin=PIPE, stderr=PIPE)
         p.communicate()
@@ -85,11 +93,31 @@ def destroy(container):
         print("Container %s: Destroying" % (container['name']))
     p = Popen(shlex.split("%s/lxc-destroy -n %s" % (config.settings['lxc-bindir'], container['name'])), stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
+def apply_lxc(container):
+    """Applies custom lxc config settings defined in profiles."""
+    if config.settings['debug']:
+        print("Container %s: Inserting lxc settings" % (container['name']))
+    fh = open("%s/%s/config" % (config.settings['lxc-rootdir'], container['name']), "a+")
+    fh.write("\n# Added for Vee\n\n")
+    for line in container['profile']['lxc_config']:
+        fh.write(line + "\n")
+    fh.close()
+
 def apply_puppet(container):
     """Apply a puppet class."""
     if config.settings['debug']:
-        print("Container %s: Applying puppet class '%s'" % (container['name'], container['profile']['puppet-class']))
+        print("Container %s: Applying puppet class '%s'" % (container['name'], container['profile']['puppet_class']))
     # TODO: Copy puppet class into container and lxc-execute -n $name puppet apply -e 'include $puppetclass'
+
+def install_puppet(container):
+    if config.settings['debug']:
+        print("Container %s: Installing puppet" % (container['name']))
+    fh = open("%s/%s/rootfs/tmp/install-puppet.sh" % (config.settings['lxc-rootdir'], container['name']),"w+")
+    os.chmod("%s/%s/rootfs/tmp/install-puppet.sh" % (config.settings['lxc-rootdir'], container['name']), stat.S_IXUSR)
+    fh.write(container['profile']['install_puppet_script'])
+    fh.close()
+
+    start(container, "/tmp/install-puppet.sh")
 
 if __name__ == "__main__":
     for container in config.containers:
